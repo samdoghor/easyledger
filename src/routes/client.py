8116d7bcbbf0cd941008d8634665f075190ea898
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-06-21 00:36:50 +0100

    clients

--- src/routes/client.py
+++ src/routes/client.py
@@ -9,4 +9,3 @@ from resources import ClientResource
 client_blueprint = Blueprint("client", __name__)
 
 client_blueprint.route("/clients")(ClientResource.client_index)
-client_blueprint.route("/clients/new_client")(ClientResource.client_add)
--- src/static/js/page.js
+++ src/static/js/page.js
@@ -1,40 +1,40 @@
-// Get the buttons and the target element
-var clientgridbtn = document.getElementById("client-grid-btn");
-var clientlistbtn = document.getElementById("client-list-btn");
-var clientgridtab = document.getElementById("client-grid-tab");
-var clientlisttab = document.getElementById("client-list-tab");
+// // Get the buttons and the target element
+// var clientgridbtn = document.getElementById("client-grid-btn");
+// var clientlistbtn = document.getElementById("client-list-btn");
+// var clientgridtab = document.getElementById("client-grid-tab");
+// var clientlisttab = document.getElementById("client-list-tab");
 
-// Add event listener to client list button
-clientlistbtn.addEventListener("click", function () {
-  clientlisttab.classList.replace("el-tab-inactive", "el-tab-active");
-  clientgridtab.classList.replace("el-tab-active", "el-tab-inactive");
+// // Add event listener to client list button
+// clientlistbtn.addEventListener("click", function () {
+//   clientlisttab.classList.replace("el-tab-inactive", "el-tab-active");
+//   clientgridtab.classList.replace("el-tab-active", "el-tab-inactive");
 
-  // Store the current state in localStorage
-  localStorage.setItem("activeTab", "list");
-});
+//   // Store the current state in localStorage
+//   localStorage.setItem("activeTab", "list");
+// });
 
-// Add event listener to client grid button
-clientgridbtn.addEventListener("click", function () {
-  clientgridtab.classList.replace("el-tab-inactive", "el-tab-active");
-  clientlisttab.classList.replace("el-tab-active", "el-tab-inactive");
+// // Add event listener to client grid button
+// clientgridbtn.addEventListener("click", function () {
+//   clientgridtab.classList.replace("el-tab-inactive", "el-tab-active");
+//   clientlisttab.classList.replace("el-tab-active", "el-tab-inactive");
 
-  // Store the current state in localStorage
-  localStorage.setItem("activeTab", "grid");
-});
+//   // Store the current state in localStorage
+//   localStorage.setItem("activeTab", "grid");
+// });
 
-// Retrieve the state from localStorage when the page loads
-window.addEventListener("load", function () {
-  const activeTab = localStorage.getItem("activeTab");
+// // Retrieve the state from localStorage when the page loads
+// window.addEventListener("load", function () {
+//   const activeTab = localStorage.getItem("activeTab");
 
-  // Check if there is a stored state and apply it
-  if (activeTab === "list") {
-    clientlisttab.classList.replace("el-tab-inactive", "el-tab-active");
-    clientgridtab.classList.replace("el-tab-active", "el-tab-inactive");
-  } else if (activeTab === "grid") {
-    clientgridtab.classList.replace("el-tab-inactive", "el-tab-active");
-    clientlisttab.classList.replace("el-tab-active", "el-tab-inactive");
-  }
-});
+//   // Check if there is a stored state and apply it
+//   if (activeTab === "list") {
+//     clientlisttab.classList.replace("el-tab-inactive", "el-tab-active");
+//     clientgridtab.classList.replace("el-tab-active", "el-tab-inactive");
+//   } else if (activeTab === "grid") {
+//     clientgridtab.classList.replace("el-tab-inactive", "el-tab-active");
+//     clientlisttab.classList.replace("el-tab-active", "el-tab-inactive");
+//   }
+// });
 
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
